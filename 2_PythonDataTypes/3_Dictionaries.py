f73dec7dfbf4f8c5f1ac99bc468da952d49c48da
Diksha Kewat <diksha.kewat>
2021-03-02 13:49:58 +0700

    Dict done

--- 2_PythonDataTypes/3_Dictionaries.py
+++ 2_PythonDataTypes/3_Dictionaries.py
@@ -42,7 +42,9 @@ del myDict['3']
 print(myDict)
 
 myDict.clear()
+print(myDict)
 del myDict
+# print(myDict)   Exception thrown
 
 
 # Dictionary Methods
@@ -50,3 +52,40 @@ myDict1 = {"1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five"}
 
 myDict2 = myDict1.copy()
 print(myDict2)
+
+
+myDict3 = {}.fromkeys([1, 2, 3], 0)
+print(myDict3)
+
+print(myDict1.get('1'))
+
+print(myDict1.items())
+
+print(myDict1.values())
+
+print(myDict1.keys())
+
+myDict1.setdefault('default', 'Ten')
+print(myDict1)
+
+myDict1.update(myDict3)
+print(myDict1)
+
+
+# Dictionary Functions
+print(all(myDict1))
+
+print(any(myDict1))
+
+print(sorted(myDict1))
+
+print(sorted(myDict1, reverse=True))
+
+print(sorted(myDict1, key=True))
+
+
+
+
+
+
+
